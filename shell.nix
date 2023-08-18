530abe67667f7ffd7f30445c48a288a44403f377
Fabio Tudone <circlespainter@users.noreply.github.com>
2023-08-18 09:46:19 +0200

    Upgrade deps (#28)

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { pkgs ? import (builtins.fetchTarball {
            # Descriptive name to make the store path easier to identify
-           name = "nixos-unstable-2023-05-18";
+           name = "nixos-unstable-2023-08-17";
            # Commit hash for nixos-unstable as of 2023-05-18
-           url = "https://github.com/nixos/nixpkgs/archive/cca2a7acb592a68c0ad6805e135ecb3b3baa6560.tar.gz";
+           url = "https://github.com/nixos/nixpkgs/archive/36e94693b7f938223a5bd149f2fb9f6417ddb676.tar.gz";
            # Hash obtained using `nix-prefetch-url --unpack <url>`
-           sha256 = "1dd9llizlnjk1z2hmxf44gxxxr41d0lfdmd2nla8f5pmk8pcndbg";
+           sha256 = "0jv9305h4h9wr4k2spyg7x3nnw22k2y8920n978pxdywhnyx4znj";
          }) {} }:
 
 with pkgs;
